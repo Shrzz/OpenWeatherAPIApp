c053d5a3015f2c1f89eb5921c84cfb0b0f57d40d
Shrzz <olegme052@gmail.com>
2021-12-09 21:01:27 +0100

    added 5 day forecast for current geolocation

--- script.js
+++ script.js
@@ -1,42 +1,69 @@
 /* Initialization */
 
-const getByCitynameButton = document.querySelector("#getByCityNameButton");
-const getByGeolocationButton = document.querySelector("#getByGeolocationButton");
-const geolocationForecastListDiv = document.querySelector(".geolocation-forecast-list .row");
-const citynameForecastListDiv = document.querySelector(".cityname-forecast-list .row");
 const weatherApiKey = config.OPEN_WEATHER_API_KEY;
 
-getByCitynameButton.addEventListener("click", () => {
-  let form = document.forms["getByCityNameButton"];
+// elements for current weather
+const getWeatherByCityNameButton = document.querySelector("#getWeatherByCityNameButton");
+const getWeatherByGeolocationButton = document.querySelector("#getWeatherByGeolocationButton");
+const geolocationWeatherListDiv = document.querySelector(".geolocation-weather-list .row");
+const citynameWeatherListDiv = document.querySelector(".cityname-weather-list .row");
+
+// elements for forecast
+const getForecastByGeolocationButton = document.querySelector("#getForecastByGeolocationButton");
+const geolocationForecastListDiv = document.querySelector(".geolocation-forecast-list .row");
+
+getWeatherByCityNameButton.addEventListener("click", () => {
+  let form = document.forms["getWeatherByCityNameButton"];
   let cityName = form.elements["cityname"].value;
-  getWeatherData(getApiSettingsStringByCity(cityName), citynameForecastListDiv);
+  processData(getSettingsStringForCityNameWeather(cityName), citynameWeatherListDiv, "weatherCityname");
 });
 
-getByGeolocationButton.addEventListener("click", () => {
+getWeatherByGeolocationButton.addEventListener("click", () => {
   let options = {
     enableHighAccuracy: true,
     timeout: 5000,
     maximumAge: 0
   };
-  navigator.geolocation.getCurrentPosition(getGeolocationSuccess, getGeolocationError, options);
+  navigator.geolocation.getCurrentPosition(getGeolocationSuccessWeather, getGeolocationError, options);
 });
 
+getForecastByGeolocationButton.addEventListener("click", () => {
+  let options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+  };
+  navigator.geolocation.getCurrentPosition(getGeolocationSuccessForecast, getGeolocationError, options);
+});
+
+
+/* -------------------------------- */
 /* OpenWeatherAPI methods */
 
 /* request data and process it */
-function getWeatherData(settingsString, targetElement) {
+function processData(settingsString, targetElement, requestType) {
   fetch(settingsString)
     .then(function (resp) { return resp.json() })
     .then(function (data) {
+      console.log(data);
       targetElement.innerHTML = "";
 
-      if (data["list"]) {
-        let dataArray = Object.values(data["list"])
-        dataArray.forEach(data => {
-          showForecastData(data, targetElement);
-        });
-      } else {
-        showForecastData(data, targetElement);
+      switch (requestType) {
+        case "weatherCityname":
+          processWeatherByCityName(data, targetElement);
+          break;
+        case "weatherGeolocation":
+          processWeatherByGeolocation(data, targetElement);
+          break;
+        case "forecastCityname":
+          processForecastByCityName(data, targetElement);
+          break;
+          case "forecastGeolocation":
+          processForecastByGeolocation(data, targetElement);
+          break;
+        default:
+          alert("Cannot process data");
+          break;
       }
     })
     .catch(function (error) {
@@ -45,18 +72,15 @@ function getWeatherData(settingsString, targetElement) {
     });
 }
 
-/* display forecast data on page in a specific format */
-function showForecastData(data, targetElement) {
-  console.log(data);
-
+/* process and display current weather data on page in a specific format */
+function processWeatherByCityName(data, targetElement) {
   let weatherArray = Object.values(data["weather"]);
   let weatherValue = weatherArray[0]["main"].toLowerCase();
 
   let colDiv = document.createElement("div");
   colDiv.classList = "col";
-
-  let arrow = 
-              `<svg height="8" style="transform: rotate(${data["wind"]["deg"] + 90}deg)" xmlns="http://www.w3.org/2000/svg" viewBox="0 10 180 80">
+  let windDirectionArrow =
+    `<svg height="8" style="transform: rotate(${data["wind"]["deg"] + 90}deg)" xmlns="http://www.w3.org/2000/svg" viewBox="0 10 180 80">
                   <defs>
                       <marker id="arrowhead" markerWidth="10" markerHeight="10" refX="0" refY="3.5" orient="auto">
                           <polygon points="0 0, 10 3.5, 0 7" fill="white"/>
@@ -66,17 +90,102 @@ function showForecastData(data, targetElement) {
               </svg> `;
 
   colDiv.innerHTML =
-                    `<div class="card text-white weather-card border-primary col-sm-12" style="background: url(${getWeatherImagePath(weatherValue)}); background-size: cover;">
+    `<div class="card text-white weather-card border-primary col-sm-12" style="background: url(${getWeatherImagePath(weatherValue)}); background-size: cover;">
                       <div class="card-body weather-card-body">
                       <h4 class="card-title temperature_header">${data["name"]}</h4>
                       <span class="card-text temperature_value">${Math.round(data["main"]["temp"])} °C, ${weatherArray[0]["description"]}</span><br/>
-                      ${arrow}<span class="card-text temperature_time ">${data["wind"]["speed"]}m/s ${getWindDirection(data["wind"]["deg"])}, ${data["main"]["pressure"]}hPa</span>
+                      ${windDirectionArrow}<span class="card-text temperature_time ">${data["wind"]["speed"]}m/s ${getWindDirection(data["wind"]["deg"])}, ${data["main"]["pressure"]}hPa</span>
                         <p class="card-text temperature_value">feels like ${Math.round(data["main"]["feels_like"])} °C</p>
-                        <p class="card-text temperature_time">${getCurrentTimeString()}</p>
+                        <p class="card-text temperature_time">${getCurrentTimeString(new Date())}</p>
                       </div>
                     </div>`;
   targetElement.appendChild(colDiv);
 }
+
+function processWeatherByGeolocation(data, targetElement) {
+  let dataArray = Object.values(data["list"])
+  dataArray.shift();
+  dataArray.forEach(data => {
+
+
+    let weatherArray = Object.values(data["weather"]);
+    let weatherValue = weatherArray[0]["main"].toLowerCase();
+
+    let colDiv = document.createElement("div");
+    colDiv.classList = "col";
+    let windDirectionArrow =
+      `<svg height="8" style="transform: rotate(${data["wind"]["deg"] + 90}deg)" xmlns="http://www.w3.org/2000/svg" viewBox="0 10 180 80">
+                  <defs>
+                      <marker id="arrowhead" markerWidth="10" markerHeight="10" refX="0" refY="3.5" orient="auto">
+                          <polygon points="0 0, 10 3.5, 0 7" fill="white"/>
+                      </marker>
+                  </defs>
+                  <line x1="0" y1="50" x2="100" y2="50" stroke="white" stroke-width="8" marker-end="url(#arrowhead)" />
+              </svg> `;
+
+    colDiv.innerHTML =
+      `<div class="card text-white weather-card border-primary col-sm-12" style="background: url(${getWeatherImagePath(weatherValue)}); background-size: cover;">
+                      <div class="card-body weather-card-body">
+                      <h4 class="card-title temperature_header">${data["name"]}</h4>
+                      <span class="card-text temperature_value">${Math.round(data["main"]["temp"])} °C, ${weatherArray[0]["description"]}</span><br/>
+                      ${windDirectionArrow}<span class="card-text temperature_time ">${data["wind"]["speed"]}m/s ${getWindDirection(data["wind"]["deg"])}, ${data["main"]["pressure"]}hPa</span>
+                        <p class="card-text temperature_value">feels like ${Math.round(data["main"]["feels_like"])} °C</p>
+                        <p class="card-text temperature_time">${getCurrentTimeString(new Date())}</p>
+                      </div>
+                    </div>`;
+    targetElement.appendChild(colDiv);
+  });
+}
+
+function processForecastByCityName(data, targetElement) {
+
+}
+
+function processForecastByGeolocation(data, targetElement) {
+  let cityName = data["city"]["name"];
+  let country = data["city"]["country"];
+  let setTime = "13:00";
+
+  // let currentWeather = data["weather"][0]["main"];
+  // let currentstr = data["weather"][0]["main"];
+
+  data["list"].forEach(element => {
+    console.log(element)
+    let timeString = getCurrentTimeString(new Date(element["dt"] * 1000));
+    let dateString = getCurrentDateString(new Date(element["dt"] * 1000));
+    console.log(dateString)
+    if (timeString == setTime){
+      let weatherValue = element["weather"][0]["main"].toLowerCase();
+      
+      let colDiv = document.createElement("div");
+      colDiv.classList = "col";
+      let windDirectionArrow =
+        `<svg height="8" style="transform: rotate(${element["wind"]["deg"] + 90}deg)" xmlns="http://www.w3.org/2000/svg" viewBox="0 10 180 80">
+                    <defs>
+                        <marker id="arrowhead" markerWidth="10" markerHeight="10" refX="0" refY="3.5" orient="auto">
+                            <polygon points="0 0, 10 3.5, 0 7" fill="white"/>
+                        </marker>
+                    </defs>
+                    <line x1="0" y1="50" x2="100" y2="50" stroke="white" stroke-width="8" marker-end="url(#arrowhead)" />
+                </svg> `;
+      colDiv.innerHTML =
+        `<div class="card text-white weather-card border-primary col-sm-12" style="background: url(${getWeatherImagePath(weatherValue)}); background-size: cover;">
+                        <div class="card-body weather-card-body">
+                        <h4 class="card-title temperature_header">${cityName}, ${dateString}</h4>
+                        <span class="card-text temperature_value">${Math.round(element["main"]["temp"])} °C, ${element["weather"][0]["description"]}</span><br/>
+                        ${windDirectionArrow}<span class="card-text temperature_time ">${element["wind"]["speed"]}m/s ${getWindDirection(element["wind"]["deg"])}, ${element["main"]["pressure"]}hPa</span>
+                          <p class="card-text temperature_value">real feel: ${Math.round(element["main"]["feels_like"])} °C</p>
+                          <p class="card-text temperature_time">${getCurrentTimeString(new Date())}</p>
+                        </div>
+                      </div>`;
+      targetElement.appendChild(colDiv);
+    }
+
+  })
+}
+
+
+
 
 /* get required photo */
 function getWeatherImagePath(weatherValue) {
@@ -149,41 +258,62 @@ function getWindDirection(degreeValue) {
 }
 
 /* get formatted time string */
-function getCurrentTimeString() {
-  let d = new Date();
-  let minutes = d.getMinutes().toString().length == 1 ? '0' + d.getMinutes() : d.getMinutes();
-  let hours = d.getHours().toString().length == 1 ? '0' + d.getHours() : d.getHours();
+function getCurrentTimeString(date) {
+  let minutes = date.getMinutes().toString().length == 1 ? '0' + date.getMinutes() : date.getMinutes();
+  let hours = date.getHours().toString().length == 1 ? '0' + date.getHours() : date.getHours();
   return `${hours}:${minutes}`;
 }
 
+function getCurrentDateString(date){
+  var mm = date.getMonth() + 1; // getMonth() is zero-based
+  var dd = date.getDate();
 
-// WNW
-// 281.25 - 303.75
+  return [(dd>9 ? '' : '0') + dd,
+          ".",
+          (mm>9 ? '' : '0') + mm,
+         ].join('');
+}
 
-// NW
-// 303.75 - 326.25
 
-// NNW
-// 326.25 - 348.75
-
-/* get api settings using city name */
-function getApiSettingsStringByCity(cityName) {
+/* get api settings string for getting current weather data using city name */
+function getSettingsStringForCityNameWeather(cityName) {
   return `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${weatherApiKey}`;
 }
 
-/* get api settings using user coordinates */
-function getApiSettingsStringByCoordinates(latitude, longitude, citiesAmount = 10) {
-  return `https://api.openweathermap.org/data/2.5/find?lat=${latitude}&lon=${longitude}&cnt=${citiesAmount}&units=metric&appid=${weatherApiKey}`;
+/* get api settings string for getting current weather near user by using user's coordinates */
+function getSettingsStringForGeolocationWeather(latitude, longitude, citiesAmount = 6) {
+  return `https://api.openweathermap.org/data/2.5/find?lat=${latitude}&lon=${longitude}&cnt=${citiesAmount + 1}&units=metric&appid=${weatherApiKey}`;
+}
+
+/* get api settings string for getting 5 day forecast using city name */
+function getSettingsStringForCityNameForecast(cityName) {
+  return `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=metric&appid=${weatherApiKey}`;
+}
+
+/* get api settings string for getting 5 day forecast for area near user using user's coordinates */
+function getSettingsStringForGeolocationForecast(latitude, longitude, stampsAmount = 40) {
+  return `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&cnt=${stampsAmount}&units=metric&appid=${weatherApiKey}`;
 }
 
 
+
+
+/* -------------------------------- */
 /* Geolocation methods */
 
-function getGeolocationSuccess(pos) {
+/* success for geolocation  */
+function getGeolocationSuccessWeather(pos) {
   let crd = pos.coords;
   let lat = crd.latitude;
   let lon = crd.longitude;
-  getWeatherData(getApiSettingsStringByCoordinates(lat, lon), geolocationForecastListDiv)
+  processData(getSettingsStringForGeolocationWeather(lat, lon), geolocationWeatherListDiv, "weatherGeolocation")
+}
+
+function getGeolocationSuccessForecast(pos) {
+  let crd = pos.coords;
+  let lat = crd.latitude;
+  let lon = crd.longitude;
+  processData(getSettingsStringForGeolocationForecast(lat, lon), geolocationForecastListDiv, "forecastGeolocation")
 }
 
 function getGeolocationError(err) {
